d68c29d525bf7ac3e21cc6b1eb271c21cda8c372
samarth-desai-18 <sdesairth018@gmail.com>
2025-02-01 16:24:59 +0530

    First Commit

---
+++ basic.java
@@ -0,0 +1,39 @@
+// import javafx.application.Application;
+// import javafx.stage.Stage;
+
+// class basic {
+//     public static void main(String[] args){
+//         // System.out.println("Hello World");
+//         // try (
+//         // Scanner sc = new Scanner(System.in)) 
+//         // {
+//         //     System.out.print("Enter a number");
+//         //     int a = sc.nextInt();
+//         //     System.out.print("Enter second number");
+//         //     int b = sc.nextInt();
+//         //     System.out.println("Sum = "+(a+b));
+//         // }
+//         // String input = "";
+//         // try (Scanner scanner = new Scanner(System.in)) {
+//         //     while (!input.equals("q")) {
+//         //         System.out.print("Input: ");
+//         //         input = scanner.nextLine();
+//         //         System.out.println("Input was : "+input);
+//         //     }
+//         //     scanner.close();
+//         // }
+//     }
+// }
+
+// public class basic {
+//     static {
+//         System.out.println("This is static block");
+//         System.exit(0);
+//     }
+// }
+
+public class basic{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
